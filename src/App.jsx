bd64194e6fbb453a7b57f97a155807b893598a4f
fredli4qooni <fredlifourqoni999@gmail.com>
2025-10-24 00:20:31 +0700

    Initial commit: Setup project with React, Vite, and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-900 min-h-screen flex justify-center items-center">
+      <h1 className="text-4xl font-bold text-white">
+        Project Kuis Siap Dimulai!
+      </h1>
+    </div>
+  )
+}
+
+export default App
